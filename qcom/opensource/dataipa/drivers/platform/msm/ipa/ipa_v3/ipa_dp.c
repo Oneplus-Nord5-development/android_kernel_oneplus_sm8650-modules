96f2832feb7f099f91e3c410ec8d8799d738e10f
anky894 <ko282ko82@gmail.com>
2025-10-08 12:07:15 +0530

    Revert "msm: ipa3: add gfp no retry flag for order-3 tmp alloc"
    This reverts commit 23aedb0d6ffe80319eaf8b7e1864018662e80727.
    
    Reason: This change causes the device to freeze when left idle with mobile data enabled.
    
    Change-Id: Idd6ba91db977b39012f97e000f550a04bc227897

--- qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
+++ qcom/opensource/dataipa/drivers/platform/msm/ipa/ipa_v3/ipa_dp.c
@@ -1,7 +1,9 @@
+
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ *
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #include <linux/ip.h>
 #include <linux/ipv6.h>
@@ -2710,11 +2712,6 @@ static struct page *ipa3_alloc_page(
 	if (unlikely(!page)) {
 		if (try_lower && p_order > 0) {
 			p_order = p_order - 1;
-			if (ipa3_ctx->gfp_no_retry) {
-				flag = GFP_KERNEL | __GFP_RETRY_MAYFAIL | __GFP_NOWARN
-					| __GFP_NOMEMALLOC;
-			}
-
 			page = __dev_alloc_pages(flag, p_order);
 			if (likely(page))
 				ipa3_ctx->stats.lower_order++;
@@ -2768,10 +2765,7 @@ static struct ipa3_rx_pkt_wrapper *ipa3_alloc_rx_pkt_page(
 	rx_pkt->page_data.page_order = sys->page_order;
 	/* For temporary allocations, avoid triggering OOM Killer. */
 	if (is_tmp_alloc) {
-		if (ipa3_ctx->gfp_no_retry)
-			flag |= __GFP_NORETRY | __GFP_NOWARN;
-		else
-			flag |= __GFP_RETRY_MAYFAIL | __GFP_NOWARN;
+		flag |= __GFP_RETRY_MAYFAIL | __GFP_NOWARN;
 		rx_pkt->page_data.page = ipa3_rmnet_alloc_page(
 			flag, &rx_pkt->page_data.page_order, true);
 	} else {
